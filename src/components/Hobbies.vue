ab464206797ad3d3907416bb07f97c35185401f2
mooseshark <61443323+mooseshark@users.noreply.github.com>
2020-10-08 00:24:24 -0400

    Hobbies Complete
    Finished updating the hobbies section for now
    Carousel seems to work the way I'd like it to on 
     web and mobile
    Text animation on the carousel could be smoother, 
     but it's functional for now.

--- src/components/Hobbies.vue
+++ src/components/Hobbies.vue
@@ -19,23 +19,6 @@
         <div class="col-md text-center">
         </div>
       </div>
-      <div class="row">
-        <div class="col-md text-center">
-          <p class="h2">
-            I swear I have hobbies, but this section is currently under
-            construction. Here's a picture of my cat for now.
-          </p>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-md text-center">
-        </div>
-        <div class="col-md text-center">
-          <img src="../assets/cat.png" class="rounded float-right mb-4" alt="it's a cat, but it's wearing a crown.">
-        </div>
-        <div class="col-md text-center">
-        </div>
-      </div>
     </div>
   </div>
 </template>
