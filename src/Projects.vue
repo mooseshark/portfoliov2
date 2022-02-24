bcfef8498dd7e1d9b3900d58b5b9aa783e98e78a
Mooseshark <61443323+mooseshark@users.noreply.github.com>
2022-02-23 19:43:56 -0500

    Cardflip Fix for FireFox
    Cardflip Fix for FireFox

---
+++ src/Projects.vue
@@ -0,0 +1,204 @@
+<template>
+  <div class="pb-4" id="projects" style="color: #ffffff; background-color: #ffffff">projects</div>
+  <div class="projects">
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="h1 text-center">Projects</div>
+          <p class="h4 text-center">
+            Old projects and projects I'm continuously updating
+          </p>
+        </div>
+      </div>
+      <div class="row mb-4">
+        <div class="col-md d-flex align-items-stretch flip-card">
+          <div class="card shadow-lg flip-card-inner">
+            <div class="card-body flip-card-front">
+              <h5 class="card-title"><strong>Untitled Game Project</strong></h5>
+              <img src="../assets/game-testing.png" class="card-img-top" alt="imgage of lttp sprites for testing in a game project">
+            </div>
+            <div class="card-body flip-card-back">
+              <h5 class="card-title"><strong>Untitled Game Project</strong></h5>
+              <p class="card-text">
+                A small adventure game project I've recently started. Currently
+                in the process of working on movement and gameplay, which is
+                why I'm using Link to the Past sprite stand-ins. Planning to do more
+                design work after testing if gameplay is fun or not.
+              </p>
+              <div class="row"><a href="" rel="noopener noreferrer" target="_blank" class="btn disabled btn-dark mb-2 pb-2"><i class="fas fa-gamepad"></i> Give it a play</a></div>
+              <div class="row"><a href="" rel="noopener noreferrer" target="_blank" class="btn disabled btn-dark"><i class="fab fa-github"></i> GitHub</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md d-flex align-items-stretch flip-card">
+          <div class="card shadow-lg flip-card-inner">
+            <div class="card-body flip-card-front">
+              <h5 class="card-title"><strong>DumbCode.dev</strong></h5>
+              <img src="../assets/dumbcode.png" class="card-img-top" alt="dumbcode.dev front page">
+            </div>
+            <div class="card-body flip-card-back">
+              <h5 class="card-title"><strong>DumbCode.dev</strong></h5>
+              <p class="card-text">
+                A tech blog for wierd little projects I build. Written mostly
+                with Vanilla JS and Bootstrap 4. This will likely be converted
+                to Vue.JS at some point. I prefer it over something like Dev.to
+                because I can easily provide working examples in posts.
+              </p>
+              <div class="row"><a href="https://dumbcode.dev/" rel="noopener noreferrer" target="_blank" class="btn btn-dark mb-2 pb-2"><i class="fas fa-book"></i> Take a look</a></div>
+              <div class="row"><a href="https://github.com/mooseshark/dumbcode" rel="noopener noreferrer" target="_blank" class="btn btn-dark"><i class="fab fa-github"></i> GitHub</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md d-flex align-items-stretch flip-card">
+          <div class="card shadow-lg flip-card-inner">
+            <div class="card-body flip-card-front">
+              <h5 class="card-title"><strong>GitHub Search in AngularJS</strong></h5>
+              <img src="../assets/angular-search.png" class="card-img-top" alt="GitHub User Search Angular homepage">
+            </div>
+            <div class="card-body flip-card-back">
+              <h5 class="card-title"><strong>GitHub Search in AngularJS</strong></h5>
+              <p class="card-text">
+                A small project written in AngularJS utilizing Angular Apollo to connect
+                to the GitHub v4 GraphQL API. It allows you to search GitHub's
+                users and view a variety of information about them. This was a
+                short used as an intro into AngularJS.
+              </p>
+              <div class="row"><a href="https://angulargithubsearch.xyz/" rel="noopener noreferrer" target="_blank" class="btn btn-dark mb-2 pb-2"><i class="fas fa-book"></i> Take a look</a></div>
+              <div class="row"><a href="https://github.com/mooseshark/angular-github-search" rel="noopener noreferrer" target="_blank" class="btn btn-dark"><i class="fab fa-github"></i> GitHub</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-md d-flex align-items-stretch flip-card">
+          <div class="card shadow-lg flip-card-inner">
+            <div class="card-body flip-card-front">
+              <h5 class="card-title"><strong>GitHub Search in Vue.js</strong></h5>
+              <img src="../assets/vue-search.png" class="card-img-top" alt="GitHub User Search Vue homepage">
+            </div>
+            <div class="card-body flip-card-back">
+              <h5 class="card-title"><strong>GitHub Search in Vue.js</strong></h5>
+              <p class="card-text">
+                A small project written in Vue.js utilizing Vue Apollo to connect
+                to the GitHub v4 GraphQL API. It allows you to search GitHub's
+                users and view a variety of information about them. This was built
+                in 5 days with no prior Vue.JS, Apollo, or GraphQL experience.
+              </p>
+              <div class="row"><a href="https://githubusersearch.xyz/#/" rel="noopener noreferrer" target="_blank" class="btn btn-dark mb-2 pb-2"><i class="fas fa-book"></i> Take a look</a></div>
+              <div class="row"><a href="https://github.com/mooseshark/github-user-search" rel="noopener noreferrer" target="_blank" class="btn btn-dark"><i class="fab fa-github"></i> GitHub</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md d-flex align-items-stretch flip-card">
+          <div class="card shadow-lg flip-card-inner">
+            <div class="card-body flip-card-front">
+              <h5 class="card-title"><strong>Button Clicks</strong></h5>
+              <img src="../assets/button-clicks.png" class="card-img-top" alt="Button Clicks homepage">
+            </div>
+            <div class="card-body flip-card-back">
+              <h5 class="card-title"><strong>Button Clicks</strong></h5>
+              <p class="card-text">
+                It's a game. A bad, unfun game, but a game none the less,
+                You click a bunch of buttons. The buttons are assigned a
+                secret value and if that value matches the number on the button
+                you lose. It's not good, but it's functional. It was build using
+                Vanilla JS and jQuery.
+
+              </p>
+              <!-- <router-link to="/button-clicks" class="navbar-brand">Button Clicks</router-link> -->
+              <div class="row"><a href="https://buttonclicks.net/" rel="noopener noreferrer" target="_blank" class="btn btn-dark mb-2 pb-2"><i class="fas fa-gamepad"></i> Give it a play</a></div>
+              <div class="row"><a href="https://github.com/mooseshark/button-clicks" rel="noopener noreferrer" target="_blank" class="btn btn-dark"><i class="fab fa-github"></i> GitHub</a></div>
+            </div>
+          </div>
+        </div>
+        <div class="col-md d-flex align-items-stretch flip-card">
+          <div class="card shadow-lg flip-card-inner">
+            <div class="card-body flip-card-front">
+              <h5 class="card-title"><strong>Space Defence Force 9</strong></h5>
+              <img src="../assets/space-defense.png" class="card-img-top" alt="space defense force 9 start screen">
+            </div>
+            <div class="card-body flip-card-back">
+              <h5 class="card-title"><strong>Space Defence Force 9</strong></h5>
+              <p class="card-text">
+                A bad Space Invaders Clone I threw together in
+                about 8 hours one night using GameMaker. It may not
+                be much, but I personally designed every asset, sound
+                and image in that 8 hours. Not too bad for a limited time
+                I think
+              </p>
+              <div class="row"><a href="https://spacedefenseforce9.com/" rel="noopener noreferrer" target="_blank" class="btn btn-dark mb-2 pb-2"><i class="fas fa-gamepad"></i> Give it a play</a></div>
+              <div class="row"><a href="https://github.com/mooseshark/space-defense-force-9" rel="noopener noreferrer" target="_blank" class="btn btn-dark"><i class="fab fa-github"></i> GitHub</a></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'projects'
+}
+</script>
+
+<style scoped>
+
+a.disabled {
+  /* Make the disabled links grayish*/
+  color: gray;
+  /* And disable the pointer events */
+  pointer-events: none;
+}
+
+.card {
+  margin: 0 auto; /* Added */
+  float: none; /* Added */
+  margin-bottom: 10px; /* Added */
+}
+
+
+.projects{
+  background-color: #e9ecef;
+}
+
+.flip-card {
+  width: 500px;
+  height: 300px;
+  background-color: transparent;
+  perspective: 1000px;
+}
+
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.6s;
+  transform-style: preserve-3d;
+  box-shadow: 0 4px 8px 0 rgba(0,0,0,0.2);
+}
+
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+.flip-card-front, .flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden;
+  backface-visibility: hidden;
+}
+
+.flip-card-front {
+  background-color: #ffffff;
+  transform: rotateX(0deg);
+}
+
+.flip-card-back {
+  transform: rotateY(180deg);
+}
+
+</style>
